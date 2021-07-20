85fffe706ed6bbd4e18c290e908bec636c3e2798
David Zhang <davidzhang@Davids-MacBook-Air.local>
2021-07-20 15:08:25 -0400

    Show Kubiquity on localhost 3000

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /mode_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
